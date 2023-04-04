5276e5a08893dfeaf0b7936445065257071a9e32
daominhkhanh20 <khanhc1k36@gmail.com>
2023-04-04 20:46:23 +0700

    update

--- test/loading/corpus_load.py
+++ test/loading/corpus_load.py
@@ -4,7 +4,7 @@ corpus = Corpus.parser_uit_squad(
     path_file='/media/dmk/D:/Data/Project/NLP/Thesis/data/UITSquad/train.json',
     mode_chunking=True,
     max_length=400,
-    overlapping_size=128
+    overlapping_size=80
 )
 print(f"Number pair question and answer: {len(corpus.list_document)}")
 print(f"Number question: {corpus.n_pair_question_answer}")
