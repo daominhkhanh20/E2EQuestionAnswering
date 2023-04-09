bc1dee8c78ff618f4c2805e6684e8666dd9eca9b
daominhkhanh20 <khanhc1k36@gmail.com>
2023-04-09 13:08:01 +0700

    update gitignore

--- test/loading/corpus_load.py
+++ test/loading/corpus_load.py
@@ -1,10 +1,10 @@
 from e2eqavn.documents import Corpus
 from e2eqavn.utils.io import load_json_data
 corpus = Corpus.parser_uit_squad(
-    path_file='/media/dmk/D:/Data/Project/NLP/Thesis/data/UITSquad/train.json',
+    path_data='/media/dmk/D:/Data/Project/NLP/Thesis/data/UITSquad/train.json',
     mode_chunking=True,
-    max_length=400,
-    overlapping_size=80
+    max_length=250,
+    overlapping_size=60
 )
 
 # corpus_dict = load_json_data('/media/dmk/D:/Data/Project/NLP/Thesis/data/UITSquad/sample.json')
