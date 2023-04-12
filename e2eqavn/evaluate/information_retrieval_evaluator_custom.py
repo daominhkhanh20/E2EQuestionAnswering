bf27a1bb07fb16dec9510283c12bea6d5859c1c1
daominhkhanh20 <khanhc1k36@gmail.com>
2023-04-12 22:09:27 +0700

    update information_evaluator_custom

--- e2eqavn/evaluate/information_retrieval_evaluator_custom.py
+++ e2eqavn/evaluate/information_retrieval_evaluator_custom.py
@@ -28,7 +28,7 @@ class InformationRetrievalEvaluatorCustom(InformationRetrievalEvaluator):
         top_k_sbert = max(max(self.mrr_at_k), max(self.ndcg_at_k), max(self.accuracy_at_k),
                           max(self.precision_recall_at_k),
                           max(self.map_at_k))
-        list_question = list(self.queries.values())
+        list_question = self.queries
         query_result_list = [[] for _ in range(len(self.queries))]
 
         results = pipeline.run(
