444d322e34b53ef97941838cdf2bb35125f33ed7
daominhkhanh20 <khanhc1k36@gmail.com>
2023-04-15 02:35:14 +0700

    change scoring

--- e2eqavn/pipeline/e2e_question_answering.py
+++ e2eqavn/pipeline/e2e_question_answering.py
@@ -31,9 +31,9 @@ class E2EQuestionAnsweringPipeline(Pipeline):
             top_k_bm25: int = 50,
             top_k_sbert: int = 10,
             **kwargs):
-        # if isinstance(queries, str):
-        #     queries = [queries]
-        #
+        if isinstance(queries, str):
+            queries = [queries]
+
         # output = self.pipeline.run(
         #     queries=queries,
         #     top_k_bm25=top_k_bm25,
