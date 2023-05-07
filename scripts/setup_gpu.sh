f8b95fa118bf97738ba5d5f1f7af81d6885c532f
daominhkhanh20 <khanhc1k36@gmail.com>
2023-05-07 22:47:09 +0700

    update setup gpu

--- scripts/setup_gpu.sh
+++ scripts/setup_gpu.sh
@@ -4,3 +4,8 @@ apt install vim -y
 cd /workspace
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh
+
+git clone https://github.com/daominhkhanh20/E2EQuestionAnswering.git
+git checkout -b develop
+pip install -r requirements.txt
+
