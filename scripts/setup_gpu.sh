105e153002199fef406e3820977b5d8f37133d80
daominhkhanh20 <khanhc1k36@gmail.com>
2023-05-07 22:51:31 +0700

    update setup gpu

--- scripts/setup_gpu.sh
+++ scripts/setup_gpu.sh
@@ -1,11 +1,14 @@
+cd /workspace
+git clone https://github.com/daominhkhanh20/E2EQuestionAnswering.git
+
 apt update
 apt upgrade -y
 apt install vim -y
-cd /workspace
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh
 
-git clone https://github.com/daominhkhanh20/E2EQuestionAnswering.git
-git checkout -b develop
-pip install -r requirements.txt
+export PATH=/workspace/miniconda3/bin:$PATH
 
+cd E2EQuestionAnswering
+git checkout -b develop3
+pip install -r requirements.txt
