6d4e4e2553759aa9ce2a36790b0264f97f598b2f
daominhkhanh20 <khanhc1k36@gmail.com>
2023-05-08 15:00:54 +0700

    update

--- e2eqavn/mrc/mrc_model.py
+++ e2eqavn/mrc/mrc_model.py
@@ -5,7 +5,7 @@ from torch import nn, Tensor
 import wandb
 import os
 from torch.utils.data import DataLoader
-from transformers import RobertaPreTrainedModel, RobertaModel
+from transformers import RobertaPreTrainedModel, RobertaModel, RobertaConfig
 from transformers.modeling_outputs import QuestionAnsweringModelOutput
 from transformers import TrainingArguments, Trainer, AutoTokenizer
 from dotenv import load_dotenv
@@ -14,63 +14,177 @@ from e2eqavn.documents import Document
 from e2eqavn.datasets import DataCollatorCustom, MRCDataset
 from e2eqavn.keywords import *
 from e2eqavn.evaluate import MRCEvaluator
+
 load_dotenv()
 wandb_api_key = os.getenv("WANDB_API")
 wandb.login(key=wandb_api_key)
 
 
-class MRCQuestionAnsweringModel(RobertaPreTrainedModel, ABC):
+#
+# class MRCQuestionAnsweringModel(RobertaPreTrainedModel, ABC):
+#     def __init__(self, config):
+#         super().__init__(config)
+#         self.num_labels = config.num_labels
+#         self.model = RobertaModel(config, add_pooling_layer=False)
+#         self.qa_outputs = nn.Linear(config.hidden_size, self.num_labels)
+#         self.init_weights()
+#
+#     def forward(self, input_ids: Tensor, attention_mask: Tensor,
+#                 start_positions: Tensor = None, end_positions: Tensor = None,
+#                 return_dict: bool = None, start_idx: Tensor = None, end_idx: Tensor = None,
+#                 words_length: Tensor = None, span_answer_ids: Tensor = None):
+#         return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+#         outputs = self.model(
+#             input_ids,
+#             attention_mask,
+#             return_dict=return_dict
+#         )
+#         sequence_output = outputs[0]
+#         batch_size = input_ids.size(0)
+#         n_sub_word = input_ids.size(1)
+#         n_word = words_length.size(1)
+#         align_matrix = torch.zeros(batch_size, n_word, n_sub_word)
+#         for i, sample_length in enumerate(words_length):
+#             for j in range(len(sample_length)):
+#                 tmp_idx = torch.sum(sample_length[:j])
+#                 align_matrix[i][j][tmp_idx: tmp_idx + sample_length[j]] = 1 if sample_length[j] > 0 else 0
+#         align_matrix = align_matrix.to(sequence_output.device)
+#         sequence_output = torch.bmm(align_matrix, sequence_output)
+#         outs = self.qa_outputs(sequence_output)
+#         start_logits, end_logits = outs.split(1, dim=-1)
+#         start_logits = start_logits.squeeze(-1).contiguous()
+#         end_logits = end_logits.squeeze(-1).contiguous()
+#         loss = None
+#         if start_positions is not None and end_positions is not None:
+#             ignore_index = start_logits.size(1)
+#             start_positions = start_positions.clamp(0, ignore_index)
+#             end_positions = end_positions.clamp(0, ignore_index)
+#             loss_fn = nn.CrossEntropyLoss(ignore_index=ignore_index)
+#             loss = (
+#                            loss_fn(start_logits, start_positions) + loss_fn(end_logits, end_positions)
+#                    ) / 2
+#         if not return_dict:
+#             output = (start_logits, end_logits) + outputs[2:]
+#             return ((loss,) + output) if loss is not None else output
+#
+#         return QuestionAnsweringModelOutput(
+#             loss=loss,
+#             start_logits=start_logits,
+#             end_logits=end_logits,
+#             hidden_states=outputs.hidden_states,
+#             attentions=outputs.attentions
+#         )
+
+class MRCQuestionAnswering(RobertaPreTrainedModel, ABC):
+    config_class = RobertaConfig
+
+    def _reorder_cache(self, past, beam_idx):
+        pass
+
+    _keys_to_ignore_on_load_unexpected = [r"pooler"]
+    _keys_to_ignore_on_load_missing = [r"position_ids"]
+
     def __init__(self, config):
         super().__init__(config)
         self.num_labels = config.num_labels
-        self.model = RobertaModel(config, add_pooling_layer=False)
-        self.qa_outputs = nn.Linear(config.hidden_size, self.num_labels)
-        self.init_weights()
 
-    def forward(self, input_ids: Tensor, attention_mask: Tensor,
-                start_positions: Tensor = None, end_positions: Tensor = None,
-                return_dict: bool = None, start_idx: Tensor = None, end_idx: Tensor = None,
-                words_length: Tensor = None, span_answer_ids: Tensor = None):
+        self.roberta = RobertaModel(config, add_pooling_layer=False)
+        self.qa_outputs = nn.Linear(config.hidden_size, config.num_labels)
+
+        # self.init_inweights()
+
+    def forward(
+            self,
+            input_ids=None,
+            words_lengths=None,
+            start_idx=None,
+            end_idx=None,
+            attention_mask=None,
+            token_type_ids=None,
+            position_ids=None,
+            head_mask=None,
+            inputs_embeds=None,
+            start_positions=None,
+            end_positions=None,
+            span_answer_ids=None,
+            output_attentions=None,
+            output_hidden_states=None,
+            return_dict=None,
+    ):
+        r"""
+        start_positions (:obj:`torch.LongTensor` of shape :obj:`(batch_size,)`, `optional`):
+            Labels for position (index) of the start of the labelled span for computing the token classification loss.
+            Positions are clamped to the length of the sequence (:obj:`sequence_length`). Position outside of the
+            sequence are not taken into account for computing the loss.
+        end_positions (:obj:`torch.LongTensor` of shape :obj:`(batch_size,)`, `optional`):
+            Labels for position (index) of the end of the labelled span for computing the token classification loss.
+            Positions are clamped to the length of the sequence (:obj:`sequence_length`). Position outside of the
+            sequence are not taken into account for computing the loss.
+        """
         return_dict = return_dict if return_dict is not None else self.config.use_return_dict
-        outputs = self.model(
+
+        outputs = self.roberta(
             input_ids,
-            attention_mask,
-            return_dict=return_dict
+            attention_mask=attention_mask,
+            token_type_ids=token_type_ids,
+            position_ids=position_ids,
+            head_mask=head_mask,
+            inputs_embeds=inputs_embeds,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
         )
+
         sequence_output = outputs[0]
-        batch_size = input_ids.size(0)
-        n_sub_word = input_ids.size(1)
-        n_word = words_length.size(1)
-        align_matrix = torch.zeros(batch_size, n_word, n_sub_word)
-        for i, sample_length in enumerate(words_length):
+
+        context_embedding = sequence_output
+
+        # Compute align word sub_word matrix
+        batch_size = input_ids.shape[0]
+        max_sub_word = input_ids.shape[1]
+        max_word = words_lengths.shape[1]
+        align_matrix = torch.zeros((batch_size, max_word, max_sub_word))
+
+        for i, sample_length in enumerate(words_lengths):
             for j in range(len(sample_length)):
-                tmp_idx = torch.sum(sample_length[:j])
-                align_matrix[i][j][tmp_idx: tmp_idx + sample_length[j]] = 1 if sample_length[j] > 0 else 0
-        align_matrix = align_matrix.to(sequence_output.device)
-        sequence_output = torch.bmm(align_matrix, sequence_output)
-        outs = self.qa_outputs(sequence_output)
-        start_logits, end_logits = outs.split(1, dim=-1)
+                start_idx = torch.sum(sample_length[:j])
+                align_matrix[i][j][start_idx: start_idx + sample_length[j]] = 1 if sample_length[j] > 0 else 0
+
+        align_matrix = align_matrix.to(context_embedding.device)
+        # Combine sub_word features to make word feature
+        context_embedding_align = torch.bmm(align_matrix, context_embedding)
+
+        logits = self.qa_outputs(context_embedding_align)
+        start_logits, end_logits = logits.split(1, dim=-1)
         start_logits = start_logits.squeeze(-1).contiguous()
         end_logits = end_logits.squeeze(-1).contiguous()
-        loss = None
+
+        total_loss = None
         if start_positions is not None and end_positions is not None:
-            ignore_index = start_logits.size(1)
-            start_positions = start_positions.clamp(0, ignore_index)
-            end_positions = end_positions.clamp(0, ignore_index)
-            loss_fn = nn.CrossEntropyLoss(ignore_index=ignore_index)
-            loss = (
-                           loss_fn(start_logits, start_positions) + loss_fn(end_logits, end_positions)
-                   ) / 2
+            # If we are on multi-GPU, split add a dimension
+            if len(start_positions.size()) > 1:
+                start_positions = start_positions.squeeze(-1)
+            if len(end_positions.size()) > 1:
+                end_positions = end_positions.squeeze(-1)
+            # sometimes the start/end positions are outside our model inputs, we ignore these terms
+            ignored_index = start_logits.size(1)
+            start_positions = start_positions.clamp(0, ignored_index)
+            end_positions = end_positions.clamp(0, ignored_index)
+            loss_fct = nn.CrossEntropyLoss(ignore_index=ignored_index)
+            start_loss = loss_fct(start_logits, start_positions)
+            end_loss = loss_fct(end_logits, end_positions)
+            total_loss = (start_loss + end_loss) / 2
+
         if not return_dict:
             output = (start_logits, end_logits) + outputs[2:]
-            return ((loss,) + output) if loss is not None else output
+            return ((total_loss,) + output) if total_loss is not None else output
 
         return QuestionAnsweringModelOutput(
-            loss=loss,
+            loss=total_loss,
             start_logits=start_logits,
             end_logits=end_logits,
             hidden_states=outputs.hidden_states,
-            attentions=outputs.attentions
+            attentions=outputs.attentions,
         )
 
 
