33201cdbe8ca240c995a5c39dc9e377b357bac41
daominhkhanh20 <khanhc1k36@gmail.com>
2023-05-16 23:19:01 +0700

    update

--- e2eqavn/datasets/MRCDataset.py
+++ e2eqavn/datasets/MRCDataset.py
@@ -36,7 +36,7 @@ class MRCDataset:
             answer_word_start_idx_key=kwargs.get(ANSWER_WORD_START_IDX, 'answer_word_start_idx'),
             answer_word_end_idx_key=kwargs.get(ANSWER_WORD_END_IDX, 'answer_word_end_idx')
         )
-        examples = qa_text_processor.make_example(corpus)
+        examples = qa_text_processor.make_example(corpus)[:100]
         dir_save = kwargs.get(FOLDER_QA_SAVE, 'data/qa')
         if not os.path.exists(dir_save):
             os.makedirs(dir_save, exist_ok=True)
@@ -55,7 +55,7 @@ class MRCDataset:
             num_proc=num_proc,
             fn_kwargs={
                 'tokenizer': tokenizer,
-                'max_length': kwargs.get(MAX_LENGTH, 368)
+                # 'max_length': kwargs.get(MAX_LENGTH, 368)
             }
         ).filter(lambda x: x['is_valid'], num_proc=num_proc)
 
