0e10fab4c74dad1ada74a0f20791eef46167f25a
daominhkhanh20 <khanhc1k36@gmail.com>
2023-07-27 00:47:03 +0700

    update

--- scripts/build_compile_model.sh
+++ scripts/build_compile_model.sh
@@ -2,7 +2,7 @@ mkdir -p model_compile/sbert
 mkdir -p model_compile/qa
 export PYTHONPATH=./
 python3 example/triton/make_triton_complie_model_qa.py
-python3 example/triton/make_triton_complie_model_sbert.py --from_mongo $1
+python3 example/triton/make_triton_complie_model_sbert.py
 
 export KAGGLE_USERNAME='daominhkhanh'
 export KAGGLE_KEY="53d2021e6812290870fc3520cbeee5ea"
