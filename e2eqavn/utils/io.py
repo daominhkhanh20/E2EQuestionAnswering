db3580d09b8aea9003276d46e3bcb8eda6e8726d
daominhkhanh20 <khanhc1k36@gmail.com>
2023-07-26 21:30:57 +0700

    update

--- e2eqavn/utils/io.py
+++ e2eqavn/utils/io.py
@@ -16,6 +16,7 @@ def load_json_data(path_file: str):
 
 def write_json_file(data: Union[Dict, List[Dict]], path_file: str):
     paths = path_file.rsplit('/')
+    logger.info(paths)
     if len(paths) == 1:
         path_file = paths[0]
     else:
