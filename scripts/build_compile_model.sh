fc6c7578e71b54ce71724ffbebf3b64a02c5fa54
daominhkhanh20 <khanhc1k36@gmail.com>
2023-07-27 00:28:35 +0700

    update

--- scripts/build_compile_model.sh
+++ scripts/build_compile_model.sh
@@ -7,6 +7,7 @@ python3 example/triton/make_triton_complie_model_sbert.py --from_mongo $1
 export KAGGLE_USERNAME='daominhkhanh'
 export KAGGLE_KEY="53d2021e6812290870fc3520cbeee5ea"
 kaggle datasets init -p model_compile
-sed -i 's/INSERT_TITLE_HERE/model-compile-v3/g' model_compile/dataset-metadata.json
-sed -i 's/INSERT_SLUG_HERE/model-compile-v3/g' model_compile/dataset-metadata.json
+MODEL_VERSION='model-compile-v4'
+sed -i "s/INSERT_TITLE_HERE/$MODEL_VERSION/g" model_compile/dataset-metadata.json
+sed -i "s/INSERT_SLUG_HERE/$MODEL_VERSION/g" model_compile/dataset-metadata.json
 kaggle datasets create -p model_compile -r zip
